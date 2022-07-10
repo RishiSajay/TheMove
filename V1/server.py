78bb1584ef431c0fcf6ddd92e99d138933174f19
RishiSajay <57403366+RishiSajay@users.noreply.github.com>
2022-07-09 23:25:37 -0400

    Add files via upload

--- V1/server.py
+++ V1/server.py
@@ -1,34 +1,118 @@
 import socket 
 import threading
+import collections
+from weakref import ref
+import string
+import random
 
+###########################################################################
+#Global variables
+
+#all votes
 votes = [0] * 10
+
+#dictionary of user/vote pairs
+dictionary = {"placeholder":-1}
+
+IDLENGTH = 5
 HEADER = 64
 PORT = 5050
-#SERVER = "192.168.1.147" command line -> ipconfig -> ipv4
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+REFRESH_MESSAGE = "!REFRESH"
+VERIFY_ID_MESSAGE = "!VERIFY_ID"
+NEW_ID_MESSAGE = "!NEW_ID_REQUEST"
+VERIFIED_MESSAGE = "!VERIFIED"
+NOT_VERIFIED_MESSAGE = "!NOT_VERIFIED"
+PLACEHOLDER = "tmp"
+###########################################################################
 
+###########################################################################
+#bind server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #make a new server
 server.bind(ADDR)
-#server specific methods
-def updateServer(msg):
-    oldVote, newVote = msg.split()
-    oldVote = int(oldVote)
-    newVote = int(newVote)
-    if (oldVote != -1):
-        votes[oldVote] -= 1
-    votes[newVote] += 1
+###########################################################################
+
+###########################################################################
+#reset votes and IDs every day
+def resetServer():
+    pass
+###########################################################################
+
+
+###########################################################################
+#user identification methods
+#users can get a new daily ID
+#old users must enter their ID to vote and get server data
+#users can request a new ID if they forgot theirs
+#all IDs are reset/removed at the end of each day
+
+#ID is stored only in server, once ID is checked, client is updated to store
+#the ID, and the ID is sent along with the vote whenever the client votes
+
+def generateUserID():
+    #characters = string.ascii_lowercase + string.digits
+    characters = string.digits
+    ID = ''.join(random.choice(characters) for i in range(IDLENGTH))
+
+    #if ID is already used
+    while (ID in dictionary):
+        ID = ''.join(random.choice(characters) for i in range(IDLENGTH))
+
+    dictionary[ID] = -1 #add user to dictionary
+    return ID
+
+def sendNewID(conn):
+    ID = generateUserID()
+    conn.send(ID.encode(FORMAT))
+
+def verifyUserID(conn, ID):
+    if ID in dictionary:
+        conn.send(VERIFIED_MESSAGE.encode(FORMAT))
+        return True
+    else: 
+        conn.send(NOT_VERIFIED_MESSAGE.encode(FORMAT))
+        return False
+###########################################################################
+
+
+
+###########################################################################
+#update votes on client and server
+
+def refreshClient(conn, clientID):
+    votesAsString = convertListToString(clientID) #votesAsString includes user's vote at the end
+    conn.send(votesAsString.encode(FORMAT)) 
+    #conn.send(str(dictionary[clientID]).encode(FORMAT))
+
+
+def updateServer(id, msg):
+    newVote = int(msg)
+    if (id in dictionary):
+        if dictionary[id] == -1: #if connected user hasn't voted, increment new vote
+            votes[newVote] += 1
+        else: 
+            votes[dictionary[id]] -= 1 #if they've voted, decrement old vote, increment new vote
+            votes[newVote] += 1
+
+    dictionary[id] = newVote #update dictionary of user and latest vote
     print(votes)
 
-def convertListToString():
+#convert server vote data to a list
+def convertListToString(clientID):
     votesAsString = ""
     for x in votes:
         votesAsString += " " + str(x)
+    votesAsString += " " + str(dictionary[clientID])
     return votesAsString
+###########################################################################
 
+
+###########################################################################
 #server client connection
+#this function handles all requests from client
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
     connected = True
@@ -37,13 +121,24 @@ def handle_client(conn, addr):
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
+            id, msg = msg.split()
             if msg == DISCONNECT_MESSAGE:
                 connected = False
+                break
+            if msg == REFRESH_MESSAGE:  #client wants to refresh votes
+                refreshClient(conn, id)
+            elif msg == NEW_ID_MESSAGE: #client wants new ID
+                sendNewID(conn)
+            elif msg == VERIFY_ID_MESSAGE: #client is logging in
+                isVerified = verifyUserID(conn, id)
+                if isVerified:
+                    #pass
+                    refreshClient(conn, id) #refresh only if client is verified
+            else: #client voted
+                updateServer(id, msg)
+                refreshClient(conn, id)
 
-            updateServer(msg)
             #print(f"[{addr}] {msg}")
-            votesAsString = convertListToString()
-            conn.send(votesAsString.encode(FORMAT))
     conn.close()
 
 
@@ -58,3 +153,4 @@ def start():
 
 print("[STARTING] server is starting...")
 start()
+###########################################################################
